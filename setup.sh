d2b6c893bad57492df43857198260949a917219b
denglert <englert.david@gmail.com>
2019-08-31 15:32:28 +0100

    Added toolbox submodule

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+BASEDIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+export QUANT_SANDBOX_DIR=${BASEDIR}
+
+PYTHON_MODULES_DIR=${QUANT_SANDBOX_DIR}/python/modules/
+export PYTHONPATH=${PYTHON_MODULES_DIR}:${PYTHONPATH}
+
+# - Source toolbox
+source ${QUANT_SANDBOX_DIR}/toolbox/setup.sh
